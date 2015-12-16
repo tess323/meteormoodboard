7f1669f94370b9b471272883516e5844e34cca60
tess323 <tess323@gmail.com>
2015-12-16 10:41:22 -0800

    finished logic

--- app.jsx
+++ app.jsx
@@ -1,7 +1,7 @@
 
 App = React.createClass({
   getInitialState() {
-    return {query: '', profession:'', color:''};
+    return {query: '', profession:'', color:'', words:''};
   },
 
   handleSubmit(event) {
@@ -11,10 +11,12 @@ App = React.createClass({
     var search = React.findDOMNode(this.refs.textInput).value.trim();
     var job = React.findDOMNode(this.refs.peopleInput).value;
     var shade = React.findDOMNode(this.refs.colorInput).value;
-    this.setState({query: search, profession: job, color: shade});
+    var message = React.findDOMNode(this.refs.wordInput).value;
+    this.setState({query: search, profession: job, color: shade, words: message});
     console.log(shade);
     console.log(search);
     console.log(job);
+    console.log(words)
  
     
  
@@ -23,19 +25,16 @@ App = React.createClass({
   },
 
   render: function() {
-    var imgSrc = "https://source.unsplash.com/1600x900/?" + this.state.query
+    var imgSrc = "https://source.unsplash.com/191x325/?" + this.state.query
     var imgProf = "https://source.unsplash.com/category/" + this.state.profession
     var imgColor= "https://source.unsplash.com/all/?" + this.state.color
+    var words = this.state.words
     return (
     <div className="container" id="boardBox">
         <div className="container">
         <header>
           <h1>Moodboard</h1>
             <form className="search" onSubmit={this.handleSubmit} >  
-              <input
-                type="text"
-                ref="textInput"
-                placeholder="What inspires you?" />
                   <select class="form-control" type="text" ref="peopleInput">
                   
                       <option value="People">People</option>
@@ -48,15 +47,25 @@ App = React.createClass({
                   </select>
                       <select class="form-control" type="text" ref="colorInput">
                       
-                          <option value="White">White</option>
-                          <option value="Black">Black</option>
-                          <option value="Red">Red</option>
-                          <option value="Blue">Blue</option>
-                          <option value="Green">Green</option>
-                          <option value="grey">Matte</option>
-                          <option value="bright">Shine</option>
+                      <option value="White">White</option>
+                      <option value="Black">Black</option>
+                      <option value="Red">Red</option>
+                      <option value="Blue">Blue</option>
+                      <option value="Green">Green</option>
+                      <option value="grey">Matte</option>
+                      <option value="bright">Shine</option>
 
                       </select>
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="What inspires you?" />
+
+               <input
+                type="text"
+                ref="wordInput"
+                placeholder="What do you want to say?" />
+                <button type="submit" className="btn btn-primary">Submit</button>
           </form>
 
 
@@ -72,14 +81,15 @@ App = React.createClass({
              <li><a href="newgrid.html"><img src={imgColor}  /></a></li>
              <li><a href="newgrid.html"><img src="https://source.unsplash.com/all/neon" /></a></li>
              <li><a href="newgrid.html"><img src="https://source.unsplash.com/random" /></a></li>
-             <li><a href="newgrid.html"><img src="https://source.unsplash.com/category/nature" /></a></li>
-             <li><a href="newgrid.html"><img src="https://source.unsplash.com/all/black" /></a></li>
+             <li id="wordbox"><a href="newgrid.html"><img src="https://source.unsplash.com/all/black" /></a></li>
              <li><a href="newgrid.html"><img src="https://source.unsplash.com/random" /></a></li>
              <li><a href="newgrid.html"><img src="https://source.unsplash.com/all/city" /></a></li>
              <li><a href="newgrid.html"><img src="https://source.unsplash.com/1600x900/?white" /></a></li>
-             <li><a href="newgrid.html"><img src="https://source.unsplash.com/random" /></a></li>
-             <li><a href="newgrid.html"><img src="https://source.unsplash.com/category/nature" /></a></li>   
-          </ul>                                    
+             <li><h1>{words}</h1></li>
+        
+          </ul> 
+          
+
           </div>
         </div>
     );
